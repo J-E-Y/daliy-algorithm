e306ceb158e2cea49fef263aa25a4321cc5ff14a
J-E-Y <rightfuljung@gmail.com>
2020-06-18 09:31:54 +0900

    :smile: programmers 17-place-str-num-order

---
+++ programmers-Kor/17-placing-str-num-order.js
@@ -0,0 +1,9 @@
+/*
+Level : 1 
+Question : https://programmers.co.kr/learn/courses/30/lessons/12917
+Source : programmers.co.kr
+*/
+
+function solution(s) {
+  return s.split("").sort().reverse().join("");
+}
