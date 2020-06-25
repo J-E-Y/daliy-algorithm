5eadc3b52206502e753e5852a7dd326c72aeb2f0
J-E-Y <rightfuljung@gmail.com>
2020-06-25 11:06:43 +0900

    :smile: programmers-finding seoul man

---
+++ programmers-Kor/19-finding-seoul-man.js
@@ -0,0 +1,11 @@
+/*
+Level : 1 
+Question : https://programmers.co.kr/learn/courses/30/lessons/12919
+Source : programmers.co.kr
+*/
+
+function solution(seoul) {
+  let index = seoul.indexOf('Kim');
+
+  return `김서방은 ${index}에 있다`;
+}
